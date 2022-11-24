994cb1533989f524ea6c7e54e31e620c318d231b
masudcodex <forester.masud@gmail.com>
2022-11-24 17:10:22 +0600

    error page design completed

--- src/Shared/ErrorPage/ErrorPage.js
+++ src/Shared/ErrorPage/ErrorPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import image from '../../Assets/Images/errorbg.jpg';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>Error</h2>
+        <div className='bg-white'>
+            <img className='md:w-9/12 lg:w-6/12 mx-auto' src={image} alt="" />
+            <p className='text-center text-xl font-semibold'>Back to<Link className='ml-4 btn btn-secondary btn-md rounded-sm text-white' to="/">Home</Link></p>
         </div>
     );
 };
