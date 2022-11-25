ca2853a688c1676e86f4bc6f92d0e17954abbe70
masudcodex <forester.masud@gmail.com>
2022-11-25 15:04:26 +0600

    category setup in the homepage with react query

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,40 +1,35 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import bedroomFurniture from '../../../Assets/Images/bedroomFurniture.jpg'
 import diningFurniture from '../../../Assets/Images/Dining furniture.jpg'
 import kitchenFurniture from '../../../Assets/Images/kitchen furniture.jpg'
+import Loader from '../../../Shared/Loader/Loader';
 
 const Categories = () => {
+    const {data: categories = [], isLoading, refetch} = useQuery({
+        queryKey: 'categories',
+        queryFn: async()=> fetch('http://localhost:5000/categories')
+        .then(res=> res.json())
+    })
+    if (isLoading) {
+        <Loader></Loader>
+    }
     return (
         <div className='py-5 bg-white'>
             <h2 className='text-3xl font-bold mt-5 ml-6 text-center pb-8'>Popular Categories</h2>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mt-10 justify-items-center mx-6'>
-                <div className="w-56 pb-10">
-                    <figure className=''>
-                        <img className='w-56 h-[150px] rounded-md' src={bedroomFurniture} alt="Shoes" />
-                        <div className="flex flex-col items-center mt-3">
-                            <h2 className="card-title">Bedroom Furniture</h2>
-                            <small className='text-center font-semibold'>2 items</small>
-                        </div>
-                    </figure>
-                </div>
-                <div className="w-56">
-                    <figure className=''>
-                        <img className='w-56 h-[150px] rounded-md' src={diningFurniture} alt="Shoes" />
-                        <div className="flex flex-col items-center mt-3">
-                            <h2 className="card-title">Dining Furniture</h2>
-                            <small className='text-center font-semibold'>2 items</small>
-                        </div>
-                    </figure>
-                </div>
-                <div className="w-56">
-                    <figure className=''>
-                        <img className='w-56 h-[150px] rounded-md' src={kitchenFurniture} alt="Shoes" />
-                        <div className="flex flex-col items-center mt-3">
-                            <h2 className="card-title">Kitchen Furniture</h2>
-                            <small className='text-center font-semibold'>2 items</small>
-                        </div>
-                    </figure>
-                </div>
+                {
+                    categories.map(category=> 
+                    <div key={category._id} className="w-56 pb-10">
+                        <figure>
+                            <img className='w-56 h-[150px] rounded-md' src={category.picture} alt="Shoes" />
+                            <div className="flex flex-col items-center mt-3">
+                                <h2 className="card-title">{category.name}</h2>
+                            </div>
+                        </figure>
+                    </div>)
+                }
+                
             </div>
         </div>
     );
